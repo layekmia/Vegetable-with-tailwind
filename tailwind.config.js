9409c699999729d9716a47bc1765438f5947d75a
Rasel Ahmed <layekofficial63@gmail.com>
2025-01-30 22:57:54 +0600

    head and service section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'body-font': ['Sora', 'serif'],
+      },
+      colors:{
+        'primary-clr': '#179800',
+        'secondary-clr': '#111111',
+        'bg-primary-clr': '#efebe3'
+      }
+    },
   },
   plugins: [],
 }
